ee3837ff8fc55f0214e6713aa48eff5a4019a803
Dmitry Zimin <yes.zimin@gmail.com>
2020-02-27 23:05:31 +0300

    2020-02-27 Финальное задание 6-го модуля

--- src/module6/AirportMap.java
+++ src/module6/AirportMap.java
@@ -26,7 +26,10 @@ public class AirportMap extends PApplet {
 	UnfoldingMap map;
 	private List<Marker> airportList;
 	List<Marker> routeList;
-	
+
+	private CommonMarker lastSelected;
+	private CommonMarker lastClicked;
+
 	public void setup() {
 		// setting up PAppler
 		size(800,600, OPENGL);
@@ -91,6 +94,37 @@ public class AirportMap extends PApplet {
 		background(0);
 		map.draw();
 		
+	}
+
+	@Override
+	public void mouseMoved() {
+
+		// clear the last selection
+		if (lastSelected != null) {
+			lastSelected.setSelected(false);
+			lastSelected = null;
+
+		}
+		selectMarkerIfHover(airportList);
+	}
+
+	// If there is a marker selected
+	private void selectMarkerIfHover(List<Marker> markers)
+	{
+		// Abort if there's already a marker selected
+		if (lastSelected != null) {
+			return;
+		}
+
+		for (Marker m : markers)
+		{
+			CommonMarker marker = (CommonMarker) m;
+			if (marker.isInside(map,  mouseX, mouseY)) {
+				lastSelected = marker;
+				marker.setSelected(true);
+				return;
+			}
+		}
 	}
 
 	public static void main(String[] args) {
--- src/module6/AirportMarker.java
+++ src/module6/AirportMarker.java
@@ -26,7 +26,27 @@ public class AirportMarker extends CommonMarker {
 	public void drawMarker(PGraphics pg, float x, float y) {
 		pg.fill(11);
 		pg.ellipse(x, y, 5, 5);
-		
+
+		if (selected) {
+
+			String strName = "Name: " + getName();
+			String strCity = "City: " + getCity();
+			String strCountry = "Country: " + getCountry();
+
+			float width = Math.max(
+					pg.textWidth(strName),
+					Math.max(pg.textWidth(strCity), pg.textWidth(strCountry)));
+
+
+			pg.fill(255);
+			pg.rect(x+10, y, width+10, 50, 5);
+
+			pg.fill(0);
+			pg.text(strName,x+12,y+12);
+			pg.text(strCity,x+12,y+27);
+			pg.text(strCountry,x+12,y+42);
+
+		}
 		
 	}
 
@@ -38,5 +58,21 @@ public class AirportMarker extends CommonMarker {
 		
 		
 	}
-	
+
+	/* Local getters for some city properties.
+	 */
+	public String getName()
+	{
+		return getStringProperty("name");
+	}
+
+	public String getCity()
+	{
+		return getStringProperty("city");
+	}
+
+	public String getCountry()
+	{
+		return getStringProperty("country");
+	}
 }
